e2d1a7d364408d1e5256fc4db99b51695636a532
T.Hori <beat3526@gmail.com>
2021-08-17 23:31:04 +0900

    AngularJSのブログ用サンプルコードを作成 DirectiveのScopeのサンプル

--- Learn-and-Understand-AngularJS/Blog/8.Directiveを自作する/app.js
+++ Learn-and-Understand-AngularJS/Blog/8.Directiveを自作する/app.js
@@ -1,14 +1,35 @@
 const sampleApp = angular.module('sampleApp', []);
 
-sampleApp.directive('sampleDirective', () => {
-    return {
-        template: '<h2>{{ ctrl.msg }}</h2>',
-        controller: 'sampleController',
-        controllerAs: 'ctrl'
-    }
-});
+sampleApp
+    .directive('sampleDirectiveA', () => {
+        return {
+            template: '<h2>{{ value }}</h2>',
+            scope: {
+                value: '='
+            }
+        }
+    })
+    .directive('sampleDirectiveB', () => {
+        return {
+            template: '<h2>{{ value }}</h2>',
+            scope: {
+                value: '@'
+            }
+        }
+    })
+    .directive('sampleDirectiveC', () => {
+        return {
+            template: '<h2>{{ function() }}</h2>',
+            scope: {
+                function: '&'
+            }
+        }
+    });
 
 sampleApp
     .controller('sampleController', [function () {
         this.msg = 'Hello, World!';
+        this.upperMsg = function () {
+            return this.msg.toUpperCase();
+        }
     }]);
