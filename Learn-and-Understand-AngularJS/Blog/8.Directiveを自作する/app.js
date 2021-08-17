17decaf110325214c1ca0d5e0217d1badbcb9745
T.Hori <beat3526@gmail.com>
2021-08-18 00:09:04 +0900

    AngularJSのブログ用サンプルコードを作成 DirectiveのScopeのサンプル コメント追加

--- Learn-and-Understand-AngularJS/Blog/8.Directiveを自作する/app.js
+++ Learn-and-Understand-AngularJS/Blog/8.Directiveを自作する/app.js
@@ -1,31 +1,6 @@
 const sampleApp = angular.module('sampleApp', []);
 
-sampleApp
-    .directive('sampleDirectiveA', () => {
-        return {
-            template: '<h2>{{ value }}</h2>',
-            scope: {
-                value: '='
-            }
-        }
-    })
-    .directive('sampleDirectiveB', () => {
-        return {
-            template: '<h2>{{ value }}</h2>',
-            scope: {
-                value: '@'
-            }
-        }
-    })
-    .directive('sampleDirectiveC', () => {
-        return {
-            template: '<h2>{{ function() }}</h2>',
-            scope: {
-                function: '&'
-            }
-        }
-    });
-
+// Directiveに渡す用の値を作るController
 sampleApp
     .controller('sampleController', [function () {
         this.msg = 'Hello, World!';
@@ -33,3 +8,30 @@ sampleApp
             return this.msg.toUpperCase();
         }
     }]);
+
+// Directive
+sampleApp
+    .directive('sampleDirectiveA', () => {
+        return {
+            template: '<h2>{{ value }}</h2>',
+            scope: {
+                value: '=' // scopeを=にすると親から渡されたその値は双方向バインディング状態になる
+            }
+        }
+    })
+    .directive('sampleDirectiveB', () => {
+        return {
+            template: '<h2>{{ value }}</h2>',
+            scope: {
+                value: '@' // scopeを@にすると文字列で渡されたことになる。
+            }
+        }
+    })
+    .directive('sampleDirectiveC', () => {
+        return {
+            template: '<h2>{{ function() }}</h2>',
+            scope: {
+                function: '&' // scopeを&にすると関数や配列を渡されたことになる。
+            }
+        }
+    });
