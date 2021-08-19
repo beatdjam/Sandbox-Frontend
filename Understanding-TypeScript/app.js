2b0be1eeb35e3496937b91d36735bf03dff1cd8a
T.Hori <beat3526@gmail.com>
2021-08-20 01:54:24 +0900

    Understanding TypeScript npm startとnpx tsc -wで自動でコンパイルされるよう設定追加

--- Understanding-TypeScript/app.js
+++ Understanding-TypeScript/app.js
@@ -1,1 +1,10 @@
-console.log('hoge!');
+function add(n1, n2) {
+    return n1 + n2;
+}
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+// let combineValues : Function;
+var combineValues;
+combineValues = add;
+console.log(combineValues(1, 2));
