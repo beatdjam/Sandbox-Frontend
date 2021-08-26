61d8adf1bae023ac4493f3fff8767264110026e9
T.Hori <beat3526@gmail.com>
2021-08-27 00:20:07 +0900

    Understanding TypeScript セクション5 クラス

--- Understanding-TypeScript/src/app.ts
+++ Understanding-TypeScript/src/app.ts
@@ -1,5 +1,6 @@
-class Department {
-    private employees: string[] = [];
+abstract class Department {
+    static staticValue = 1;
+    protected employees: string[] = [];
 
     constructor(private readonly id: string, private readonly name: string) {
     }
@@ -16,6 +17,12 @@ class Department {
         console.log(this.employees.length);
         console.log(this.employees);
     }
+
+    static createEmployee(name: string) {
+        return {name: name};
+    }
+
+    abstract hoge(): void;
 }
 
 class ITDepartment extends Department {
@@ -25,32 +32,82 @@ class ITDepartment extends Department {
         super(id, 'IT');
         this.admins = admins;
     }
+
+    override hoge() {
+    }
 }
 
 
 class AccountingDepartment extends Department {
+    private lastReport: string | undefined;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('レポートが見つかりません');
+    }
+
+    set mostRecentReport(value: string) {
+        if (value) {
+            this.reports.push(value);
+        }
+        throw new Error('エラー！');
+    }
+
     constructor(id: string, private reports: string[]) {
         super(id, 'Accounting');
+        this.lastReport = reports[0];
     }
 
     addReport(text: string) {
         this.reports.push(text);
+        this.lastReport = text;
     }
 
     printReports() {
         console.log(this.reports);
+    }
+
+    override addEmployee(employee: string) {
+        if (employee === 'Max') {
+            return;
+        }
+
+        this.employees.push(employee);
+        // super.addEmployee(employee);
+    }
+
+    override hoge() {
     }
 }
 
 
 const it = new ITDepartment('d1', ['Max']);
 console.log(it);
-const accounting = new AccountingDepartment('d1', ['Max']);
-accounting.addReport('Something');
-accounting.printReports();
-console.log(accounting);
-
 it.describe();
 it.addEmployee('taro');
 it.addEmployee('jiro');
 it.printEmployeeInformation();
+
+const accounting = new AccountingDepartment('d1', ['Max']);
+console.log(accounting.mostRecentReport);
+accounting.addReport('Something');
+accounting.printReports();
+console.log(accounting);
+console.log(accounting.mostRecentReport);
+
+// シングルトン
+class Hoge {
+    private static instance: Hoge;
+
+    private constructor() {
+    }
+
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        return new Hoge();
+    }
+}
