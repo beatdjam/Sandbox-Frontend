400a40bba478115ee029ea81701a74d918558971
T.Hori <beat3526@gmail.com>
2021-08-29 23:57:00 +0900

    Understanding TypeScript セクション6 交差型

--- Understanding-TypeScript/src/app.ts
+++ Understanding-TypeScript/src/app.ts
@@ -1,60 +1,22 @@
-// interface Person {
-//     name: string;
-//     age: number;
-//
-//     greet(phrase: string): void;
-// }
-
-// interfaceと似たようなことができるけど、interfaceはオブジェクトの表現に特化している
-// type Person = {
-//     name: string;
-//     age: number;
-//
-//     greet(phrase: string): void;
-// }
-
-// 関数のカスタム型と同じようなこともできる
-// type AddFn = (a: number, b: number) => number;
-interface AddFn {
-    (n1: number, n2: number): number;
+// 交差型(Intersection Types)
+type Admin = {
+    name: string;
+    privileges: string[]
 }
 
-let add: AddFn;
-add = (n1: number, n2: number) => n1 + n2;
-
-interface Named {
-    readonly name: string;
-    outputName?: string; // Optional
+type Employee = {
+    name: string;
+    startDate: Date;
 }
 
-interface Greetable extends Named {
-    greet(phrase: string): void;
+type ElevatedEmployee = Admin & Employee
+
+const e1: ElevatedEmployee = {
+    name: '',
+    privileges: ['creat-server'],
+    startDate: new Date(),
 }
 
-class Person implements Greetable {
-    constructor(public name: string, public readonly age: number) {
-    }
-
-    greet(phrase: string): void {
-        console.log(phrase + ' ' + this.name);
-    }
-
-}
-
-let user: Person;
-
-user = {
-    name: 'Max',
-    age: 30,
-
-    greet(phrase: string) {
-        console.log(phrase + ' ' + this.name);
-    }
-}
-
-user.greet('Hello');
-
-let user1: Greetable;
-user1 = new Person('Max', 30);
-user1.greet('Hello I am');
-console.log(user1);
+type Combinable = string | boolean;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
