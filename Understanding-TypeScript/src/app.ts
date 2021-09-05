fdf10c9192bfaf761ecd3cdc80aeeb7242c1cbde
T.Hori <beat3526@gmail.com>
2021-09-05 23:31:56 +0900

    Understanding TypeScript セクション8 デコレータ

--- Understanding-TypeScript/src/app.ts
+++ Understanding-TypeScript/src/app.ts
@@ -4,7 +4,31 @@ function Logger(constructor: Function) {
     console.log(constructor);
 }
 
+function LoggerFactory(str : string){
+    return function(constructor: Function) {
+        console.log('ログ出力 + str' + str);
+        console.log(constructor);
+    }
+}
+
+// template: dom
+// hookId: Id
+function WithTemplate(template: string, hookId: string) {
+    return function (constructor: any) {
+        const hookEl = document.getElementById(hookId);
+
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            const p = new constructor();
+            hookEl.querySelector('h1')!.textContent = p.name;
+        }
+    }
+}
+
+// 複数のデコレータを定義すると下から実行される
 @Logger
+@LoggerFactory('hoge')
+@WithTemplate('', 'app')
 class Person {
     name = 'Max';
 
