45551dc7ad95714f4c72bc24e4213f7f3d5fffdf
T.Hori <beat3526@gmail.com>
2021-09-06 23:33:30 +0900

    Understanding TypeScript セクション8 デコレータ

--- Understanding-TypeScript/src/app.ts
+++ Understanding-TypeScript/src/app.ts
@@ -1,11 +1,11 @@
-// デコレータ
+// クラスデコレータ
 function Logger(constructor: Function) {
     console.log('ログ出力');
     console.log(constructor);
 }
 
-function LoggerFactory(str : string){
-    return function(constructor: Function) {
+function LoggerFactory(str: string) {
+    return function (constructor: Function) {
         console.log('ログ出力 + str' + str);
         console.log(constructor);
     }
@@ -25,7 +25,6 @@ function WithTemplate(template: string, hookId: string) {
     }
 }
 
-// 複数のデコレータを定義すると下から実行される
 @Logger
 @LoggerFactory('hoge')
 @WithTemplate('', 'app')
@@ -39,3 +38,54 @@ class Person {
 
 const pers = new Person();
 console.log(pers);
+
+// プロパティデコレータ
+function Log(target: any, propertyName: string | Symbol) {
+    console.log(target, propertyName);
+}
+
+// アクセサデコレータ
+function Log2(target: any, name: string, descripter: PropertyDescriptor) {
+    console.log(target);
+    console.log(name);
+    console.log(descripter);
+}
+
+// メソッドデコレータ
+function Log3(target: any, name: string | Symbol, descripter: PropertyDescriptor) {
+    console.log(target);
+    console.log(name);
+    console.log(descripter);
+}
+
+// パラメータデコレータ
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    @Log2
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        } else {
+            throw new Error('invalid number');
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
+        return this._price * (1 + tax);
+    }
+}
