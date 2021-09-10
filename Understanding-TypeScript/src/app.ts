eeff13aebbfe7b7fdf92d7b187711ab3ec46c872
T.Hori <beat3526@gmail.com>
2021-09-10 23:57:43 +0900

    Understanding TypeScript セクション8 デコレータによるバリデーション

--- Understanding-TypeScript/src/app.ts
+++ Understanding-TypeScript/src/app.ts
@@ -14,7 +14,7 @@ function LoggerFactory(str: string) {
 // template: dom
 // hookId: Id
 function WithTemplate(template: string, hookId: string) {
-    return function<T extends {new(..._: any[]): {name: string}} > (originConstructor: T) {
+    return function <T extends { new(..._: any[]): { name: string } }>(originConstructor: T) {
         // デコレータを呼び出した
         return class extends originConstructor {
             constructor(..._: any[]) {
@@ -96,7 +96,7 @@ class Product {
 }
 
 // メソッドデコレータでthisの挙動を変える
-function AutoBind(_: any,_2: string, descripter : PropertyDescriptor) {
+function AutoBind(_: any, _2: string, descripter: PropertyDescriptor) {
     const originMethod = descripter.value;
     const adjDescripter: PropertyDescriptor = {
         configurable: true,
@@ -110,7 +110,7 @@ function AutoBind(_: any,_2: string, descripter : PropertyDescriptor) {
 }
 
 class Printer {
-    message= 'クリックしました';
+    message = 'クリックしました';
 
     @AutoBind
     showMessage() {
@@ -121,4 +121,76 @@ class Printer {
 const p = new Printer();
 
 const button = document.querySelector('button')!;
-button.addEventListener('click',p.showMessage);
+button.addEventListener('click', p.showMessage);
+
+interface ValidatorConfig {
+    [prop: string]: {
+        [validatableProp: string]: string[]
+    }
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+        [propName] : ['required'],
+    }
+}
+
+function PositiveNumber(target: any, propName: string) {
+    registeredValidators[target.constructor.name] = {
+        ...registeredValidators[target.constructor.name],
+        [propName] : ['positive'],
+    }
+}
+
+function validate(obj: object) {
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if(!objValidatorConfig) {
+        return true;
+    }
+
+    let isValid = true;
+    for (const prop in objValidatorConfig) {
+        for(const validator of objValidatorConfig[prop]) {
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop];
+                    break;
+                case 'positive':
+                    isValid = isValid &&  obj[prop] > 0;
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
+
+class Course {
+    @Required
+    title: string;
+    @PositiveNumber
+    price: number;
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this.price = p;
+    }
+}
+
+const courceForm = document.querySelector('form')!;
+courceForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title') as HTMLInputElement;
+    const priceEl = document.getElementById('price') as HTMLInputElement;
+
+    const title = titleEl.value;
+    const price = +priceEl.value;
+
+    const createdCource = new Course(title, price);
+    if (!validate(createdCource)) {
+        alert('Error');
+        return;
+    }
+    console.log(createdCource);
+});
