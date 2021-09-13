47059842a73ae64d790a82581e26dce7cb3446ff
T.Hori <beat3526@gmail.com>
2021-09-14 00:56:53 +0900

    Understanding TypeScript セクション9 入力のバリデーション

--- Understanding-TypeScript/src/app.ts
+++ Understanding-TypeScript/src/app.ts
@@ -8,6 +8,50 @@ function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor): PropertyD
     }
 }
 
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+
+class ValidatableImpl {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+
+    constructor(args: Validatable) {
+        this.value = args.value;
+        this.required = args.required;
+        this.minLength = args.minLength;
+        this.maxLength = args.maxLength;
+        this.min = args.min;
+        this.max = args.max;
+    }
+
+    isValid(): boolean {
+        if (this.required && this.value.toString().trim().length === 0 || isNaN(+this.value)) return false;
+
+        if (typeof this.value === 'string') {
+            if (this.minLength != null && this.value.length < this.minLength) return false;
+            if (this.maxLength != null && this.value.length > this.maxLength) return false;
+        }
+
+        if (typeof this.value === 'number') {
+            if (this.min != null && this.value < this.min) return false;
+            if (this.max != null && this.value > this.max) return false;
+        }
+
+        return true;
+    }
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -35,10 +79,40 @@ class ProjectInput {
         this.configure();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enterdManday = this.mandayInputElement.value;
+
+        // validation
+        const validate = [
+            new ValidatableImpl({value: enteredTitle, required: true}),
+            new ValidatableImpl({value: enteredDescription, required: true, minLength: 5}),
+            new ValidatableImpl({value: +enterdManday, required: true, minLength: 5}),
+        ]
+        if (!validate.every(it => it.isValid())) {
+            alert('Invalid Input');
+            return;
+        }
+
+        return [enteredTitle, enteredDescription, parseFloat(enterdManday)];
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.mandayInputElement.value = '';
+    }
+
     @AutoBind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, manday] = userInput;
+            console.log(title, desc, manday);
+            this.clearInputs();
+        }
     }
 
     private configure() {
