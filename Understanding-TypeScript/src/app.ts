21a809afc21b33237ca3bc4da7e57a50008d1795
T.Hori <beat3526@gmail.com>
2021-09-15 00:39:58 +0900

    Understanding TypeScript セクション9 ListのDOM要素の追加まで

--- Understanding-TypeScript/src/app.ts
+++ Understanding-TypeScript/src/app.ts
@@ -36,20 +36,101 @@ class ValidatableImpl {
     }
 
     isValid(): boolean {
-        if (this.required && this.value.toString().trim().length === 0 || isNaN(+this.value)) return false;
 
-        if (typeof this.value === 'string') {
+
+        if (typeof this.value === `string`) {
+            if (this.required && this.value.toString().trim().length === 0) return false;
             if (this.minLength != null && this.value.length < this.minLength) return false;
             if (this.maxLength != null && this.value.length > this.maxLength) return false;
         }
 
-        if (typeof this.value === 'number') {
+        if (typeof this.value === `number`) {
+            if (this.required && isNaN(+this.value)) return false;
             if (this.min != null && this.value < this.min) return false;
             if (this.max != null && this.value > this.max) return false;
         }
 
         return true;
     }
+}
+
+class ProjectState {
+    private listeners: any[] = [];
+    private projects: any[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+    }
+
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        this.instance = new ProjectState();
+        return this.instance;
+    }
+
+    addProject(title: string, description: string, manday: number) {
+        const newProject = {
+            id: Math.random().toString(),
+            title: title,
+            description: description,
+            manday: manday
+        }
+        this.projects.push(newProject);
+        this.listeners.forEach(it => it(this.projects.slice()));
+    }
+
+    addListener(fn: Function) {
+        this.listeners.push(fn);
+    }
+}
+
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+    assignedProjects: any[];
+
+    constructor(private type: `active` | `finished`) {
+        // template読み込み
+        this.templateElement = document.getElementById(`project-list`)! as HTMLTemplateElement;
+        // app本体のdiv読み込み
+        this.hostElement = document.getElementById(`app`)! as HTMLDivElement;
+        this.assignedProjects = [];
+
+        // templateの適用
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = `${this.type}-projects`;
+
+        ProjectState.getInstance().addListener((projects: any[]) => {
+            this.assignedProjects = projects;
+            this.renderProjects();
+        });
+
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderProjects() {
+        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        this.assignedProjects.forEach(item => {
+            const listItem = document.createElement(`li`);
+            listItem.textContent = item.title;
+            listEl.appendChild(listItem);
+        });
+    }
+
+    private renderContent() {
+        this.element.querySelector(`ul`)!.id = `${this.type}-projects-list`;
+        this.element.querySelector(`h2`)!.textContent = this.type === `active` ? `実行中` : `完了`;
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement(`beforeend`, this.element);
+    }
+
 }
 
 class ProjectInput {
@@ -62,20 +143,20 @@ class ProjectInput {
 
     constructor() {
         // template読み込み
-        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.templateElement = document.getElementById(`project-input`)! as HTMLTemplateElement;
         // app本体のdiv読み込み
-        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        this.hostElement = document.getElementById(`app`)! as HTMLDivElement;
 
         // templateの適用
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
-        this.element.id = 'user-input';
+        this.element.id = `user-input`;
         this.attach();
 
         // 講義ではattachより先にconfigureしてたけど、attachしないとDOMが無いのでnullになる
-        this.titleInputElement = document.getElementById('title')! as HTMLInputElement;
-        this.descriptionInputElement = document.getElementById('description')! as HTMLInputElement;
-        this.mandayInputElement = document.getElementById('manday')! as HTMLInputElement;
+        this.titleInputElement = document.getElementById(`title`)! as HTMLInputElement;
+        this.descriptionInputElement = document.getElementById(`description`)! as HTMLInputElement;
+        this.mandayInputElement = document.getElementById(`manday`)! as HTMLInputElement;
         this.configure();
     }
 
@@ -85,13 +166,13 @@ class ProjectInput {
         const enterdManday = this.mandayInputElement.value;
 
         // validation
-        const validate = [
-            new ValidatableImpl({value: enteredTitle, required: true}),
-            new ValidatableImpl({value: enteredDescription, required: true, minLength: 5}),
-            new ValidatableImpl({value: +enterdManday, required: true, minLength: 5}),
+        const validate: Validatable[] = [
+            {value: enteredTitle, required: true},
+            {value: enteredDescription, required: true, minLength: 5},
+            {value: +enterdManday, required: true, min: 1, max: 1000},
         ]
-        if (!validate.every(it => it.isValid())) {
-            alert('Invalid Input');
+        if (!validate.every(it => new ValidatableImpl(it).isValid())) {
+            alert(`Invalid Input`);
             return;
         }
 
@@ -99,9 +180,9 @@ class ProjectInput {
     }
 
     private clearInputs() {
-        this.titleInputElement.value = '';
-        this.descriptionInputElement.value = '';
-        this.mandayInputElement.value = '';
+        this.titleInputElement.value = ``;
+        this.descriptionInputElement.value = ``;
+        this.mandayInputElement.value = ``;
     }
 
     @AutoBind
@@ -110,18 +191,20 @@ class ProjectInput {
         const userInput = this.gatherUserInput();
         if (Array.isArray(userInput)) {
             const [title, desc, manday] = userInput;
-            console.log(title, desc, manday);
+            ProjectState.getInstance().addProject(title, desc, manday);
             this.clearInputs();
         }
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler);
+        this.element.addEventListener(`submit`, this.submitHandler);
     }
 
     private attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+        this.hostElement.insertAdjacentElement(`afterbegin`, this.element);
     }
 }
 
-const prjInput = new ProjectInput();
+const input = new ProjectInput();
+const active = new ProjectList(`active`);
+const finished = new ProjectList(`finished`);
