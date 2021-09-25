b39013e7f72f247e42a1b961a373e7377513b2fd
T.Hori <beat3526@gmail.com>
2021-09-26 01:41:57 +0900

    Understanding TypeScript セクション11 実装ファイルを移動

---
+++ Understanding-TypeScript/sample/8-11-sample-project/src/app.ts
@@ -0,0 +1,7 @@
+import {ProjectInput} from "./components/project-input-component";
+import {ProjectList} from "./components/project-list-component";
+import {ProjectStatus} from "./models/project-model";
+
+new ProjectInput();
+new ProjectList(ProjectStatus.Active);
+new ProjectList(ProjectStatus.Finished);
--- Understanding-TypeScript/src/app.ts
+++
@@ -1,7 +0,0 @@
-import {ProjectInput} from "./components/project-input-component";
-import {ProjectList} from "./components/project-list-component";
-import {ProjectStatus} from "./models/project-model";
-
-new ProjectInput();
-new ProjectList(ProjectStatus.Active);
-new ProjectList(ProjectStatus.Finished);
