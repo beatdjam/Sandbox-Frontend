16a61652daad24c0b501f039a61c5356d517f6f5
T.Hori <beat3526@gmail.com>
2021-10-08 00:01:34 +0900

    Angular入門 セクション4 社員表示コンポーネントの実装

--- Introducting-Angular/admin-app/src/app/app.module.ts
+++ Introducting-Angular/admin-app/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
