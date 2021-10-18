21d5b50fa0c9dcd86fa26f5af9f101ad52fe6959
T.Hori <beat3526@gmail.com>
2021-10-18 23:10:49 +0900

    Angular入門 セクション4 更新実装

--- Introducting-Angular/admin-app/src/app/member.service.ts
+++ Introducting-Angular/admin-app/src/app/member.service.ts
@@ -1,14 +1,16 @@
 import {Injectable} from '@angular/core';
 import {Member} from "./member";
-import {MEMBERS} from "./mock-members";
 import {Observable, of} from "rxjs";
 import {MessageService} from "./message.service";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {catchError, tap} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class MemberService {
   private membersUrl = 'api/members';
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  }
 
   constructor(private messageService: MessageService, private httpService: HttpClient) {
   }
@@ -16,14 +18,25 @@ export class MemberService {
   getMembers(): Observable<Member[]> {
     return this.httpService.get<Member[]>(this.membersUrl)
       .pipe(
-        tap(members => this.log('社員データを取得しました')),
+        tap(_ => this.log('社員データを取得しました')),
         catchError(this.handleError<Member[]>('getMembers', []))
       );
   }
 
   getMember(id: number): Observable<Member | undefined> {
-    this.log(`社員${id}を取得`);
-    return of(MEMBERS.find(member => member.id === id));
+    const url = `${this.membersUrl}/${id}`;
+    return this.httpService.get<Member>(url).pipe(
+      tap(_ => this.log('社員データを取得しました')),
+      catchError(this.handleError<Member>(`getMember id=${id}`))
+    );
+  }
+
+  updateMember(member: Member | undefined): Observable<any> {
+    return this.httpService.put<Member>(this.membersUrl, member, this.httpOptions)
+      .pipe(
+        tap(_ => this.log('社員データを変更しました')),
+        catchError(this.handleError<any>(`updateMember`))
+      )
   }
 
   private log(message: string) {
--- Introducting-Angular/admin-app/src/app/mock-members.ts
+++
@@ -1,9 +0,0 @@
-import {Member} from "./member";
-
-export const MEMBERS: Member[] = [
-  {id: 1, name: '一郎'},
-  {id: 2, name: '次郎'},
-  {id: 3, name: '三郎'},
-  {id: 4, name: '四郎'},
-  {id: 5, name: '五郎'},
-];
