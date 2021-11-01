7dccede9437fd5383201c9ffac5c832dad4d6baa
T.Hori <beat3526@gmail.com>
2021-11-01 23:20:38 +0900

    Angular入門 セクション7 コメント表示のモックデータを作成

--- Introducting-Angular/angular-chat/src/app/app.component.ts
+++ Introducting-Angular/angular-chat/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import {Component} from '@angular/core';
 import {Comment} from "./class/comment";
+import {User} from "./class/user";
+
+
+const CURRENT_USER = new User(1, 'test1');
+const ANOTHER_USER = new User(2, 'test2');
 
 const COMMENTS: Comment[] = [
-  {name: 'A', message: 'message1'},
-  {name: 'A', message: 'message2'},
-  {name: 'B', message: 'message3'},
-]
+  new Comment(ANOTHER_USER, 'message1'),
+  new Comment(ANOTHER_USER, 'message2'),
+  new Comment(CURRENT_USER, 'message3'),
+  new Comment(CURRENT_USER, 'message4'),
+];
 
 @Component({
   selector: 'ac-root',
@@ -14,4 +20,5 @@ const COMMENTS: Comment[] = [
 })
 export class AppComponent {
   comments = COMMENTS;
+  currentUser = CURRENT_USER;
 }
