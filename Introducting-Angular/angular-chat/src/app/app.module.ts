899ccea84c5ead74a3ffbb6e5350d2f6a60d0af3
T.Hori <beat3526@gmail.com>
2021-11-05 23:48:41 +0900

    Angular入門 セクション7 カスタムパイプの作成

--- Introducting-Angular/angular-chat/src/app/app.module.ts
+++ Introducting-Angular/angular-chat/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { CommentDatePipe } from './pipes/comment-date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentDatePipe
   ],
   imports: [
     BrowserModule,
