facfe0351f8ebecd64670f372beedf29944541f9
T.Hori <beat3526@gmail.com>
2021-11-10 23:43:39 +0900

    Angular入門 セクション7 投稿の保持をRealtimeDatabaseへ変更

--- Introducting-Angular/angular-chat/src/app/app.component.ts
+++ Introducting-Angular/angular-chat/src/app/app.component.ts
@@ -2,18 +2,11 @@ import {Component} from '@angular/core';
 import {Comment} from "./class/comment";
 import {User} from "./class/user";
 import {Observable} from "rxjs";
-import {AngularFireDatabase} from "@angular/fire/database";
+import {AngularFireDatabase, AngularFireList} from "@angular/fire/database";
 
 
 const CURRENT_USER = new User(1, 'test1');
-const ANOTHER_USER = new User(2, 'test2');
-
-const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'message1'),
-  new Comment(ANOTHER_USER, 'message2'),
-  new Comment(CURRENT_USER, 'message3'),
-  new Comment(CURRENT_USER, 'message4'),
-];
+// const ANOTHER_USER = new User(2, 'test2');
 
 @Component({
   selector: 'ac-root',
@@ -21,19 +14,21 @@ const COMMENTS: Comment[] = [
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  comments = COMMENTS;
+  comments$: Observable<Comment[]>;
+  commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
-  comment = '';
-  item$: Observable<any>;
+  input = '';
 
   constructor(private db: AngularFireDatabase) {
-    this.item$ = db.object('/item').valueChanges();
+    this.commentsRef = db.list('/comments');
+    this.comments$ = this.commentsRef.valueChanges();
   }
 
 
   addComment(comment: string): void {
     if (comment) {
-      this.comments.push(new Comment(this.currentUser, comment));
+      this.commentsRef.push(new Comment(this.currentUser, comment));
+      this.input = '';
     }
   }
 }
