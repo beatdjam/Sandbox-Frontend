586e1a0be6d70f5e9a1f0090c952bf9b21249a80
T.Hori <beat3526@gmail.com>
2021-11-15 21:51:21 +0900

    Angular入門 セクション7 モジュール分割

---
+++ Introducting-Angular/angular-chat/src/app/core/core.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AngularFireModule} from "@angular/fire";
+import {AngularFireAuthModule} from "@angular/fire/auth";
+import {AngularFireDatabaseModule} from "@angular/fire/database";
+import {environment} from "../../environments/environment";
+import {HeaderComponent} from './components/header/header.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+  ],
+})
+export class CoreModule {
+}
