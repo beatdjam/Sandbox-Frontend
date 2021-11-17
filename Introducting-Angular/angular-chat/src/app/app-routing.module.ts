ccd0d1dafad98eb36afa20b3c78e61f3099dcde8
T.Hori <beat3526@gmail.com>
2021-11-17 23:46:03 +0900

    Angular入門 セクション7 アカウント登録画面追加

--- Introducting-Angular/angular-chat/src/app/app-routing.module.ts
+++ Introducting-Angular/angular-chat/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {ChatComponent} from "./chat/chat.component";
 import {NotFoundComponent} from "./core/components/not-found/not-found.component";
+import {SignUpComponent} from "./sign-up/sign-up.component";
 const routes: Routes  = [
   {path: '', component: ChatComponent},
+  {path: 'signup', component: SignUpComponent},
   {path: '**', component: NotFoundComponent},
 ]
 @NgModule({
