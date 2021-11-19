4974f972a5022bcff895ac2ac3b0ae3527011471
T.Hori <beat3526@gmail.com>
2021-11-19 23:48:18 +0900

    Angular入門 セクション7 認証メール送信実装

--- Introducting-Angular/angular-chat/src/app/core/services/auth.service.ts
+++ Introducting-Angular/angular-chat/src/app/core/services/auth.service.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@angular/core';
-import { AngularFireAuth}  from "@angular/fire/auth";
-import * as firebase from "firebase";
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth) {
+  }
 
-  create(email: string, password: string): Promise<firebase.auth.UserCredential> {
-    return this.afAuth.createUserWithEmailAndPassword(email, password);
+  create(email: string, password: string): Promise<void> {
+    return this.afAuth.createUserWithEmailAndPassword(email, password)
+      .then((credential) => {
+        const {user} = credential;
+        const actionCodeSettings = {url: `http://localhost:4200/?newAccount=true&user=${user.uid}`}
+        return credential.user.sendEmailVerification(actionCodeSettings);
+      });
   }
 }
