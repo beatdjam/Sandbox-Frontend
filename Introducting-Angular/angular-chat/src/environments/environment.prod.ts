62d55976d84d5110ea793fed331f520deddc5d51
T.Hori <beat3526@gmail.com>
2021-11-29 00:15:09 +0900

    Angular入門 セクション7 Firebase Hostingに公開する設定を追加

--- Introducting-Angular/angular-chat/src/environments/environment.prod.ts
+++ Introducting-Angular/angular-chat/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCWPWcuISxoYyZLEQZn0pMLquVxnMDtqDY",
+    authDomain: "angular-chat-6c1d3.firebaseapp.com",
+    projectId: "angular-chat-6c1d3",
+    storageBucket: "angular-chat-6c1d3.appspot.com",
+    messagingSenderId: "447021498460",
+    appId: "1:447021498460:web:95b2f76388898100bda699"
+  }
 };
