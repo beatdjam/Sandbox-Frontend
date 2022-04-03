4fce0c8171aea37ecf930aafb4e5023a91b13c0d
T.Hori <beat3526@gmail.com>
2022-04-04 01:05:36 +0900

    Angular After Tutorial 1-2. フォームと状態管理 commit漏れ修正

---
+++ angular-after-tutorial/src/app/user-list-filter/user-list-filter.component.ts
@@ -0,0 +1,40 @@
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {UserListFilter} from "../state";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {takeUntil} from "rxjs";
+
+@Component({
+  selector: 'app-user-list-filter',
+  templateUrl: './user-list-filter.component.html'
+})
+export class UserListFilterComponent implements OnInit, OnDestroy {
+  @Input() set value(value: UserListFilter) {
+    this.setFormValue(value);
+  }
+
+  @Output() valueChange = new EventEmitter<UserListFilter>();
+
+  form: FormGroup;
+
+  private onDestroy = new EventEmitter();
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nameFilter: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.form.valueChanges.pipe(takeUntil(this.onDestroy)).subscribe(value => {
+      this.valueChange.emit(value);
+    });
+  }
+
+  ngOnDestroy() {
+    this.onDestroy.unsubscribe();
+  }
+
+  private setFormValue(value: UserListFilter) {
+    this.form.setValue(value, { emitEvent: false });
+  }
+}
