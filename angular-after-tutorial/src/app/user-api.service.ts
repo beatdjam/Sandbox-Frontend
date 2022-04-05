00499f5179fa69f9506f6f694033f841cf391f1c
T.Hori <beat3526@gmail.com>
2022-04-05 23:16:49 +0900

    Angular After Tutorial 1-3. ルーティングと状態管理 APIリクエスト用のServiceを作成

---
+++ angular-after-tutorial/src/app/user-api.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {firstValueFrom, map} from "rxjs";
+import {User} from "./user";
+
+const apiHost = 'https://reqres.in/api';
+
+interface ApiResponse<T> {
+  data: T;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserApiService {
+  constructor(private http: HttpClient) {
+  }
+
+  getAllUsers(): Promise<User[]> {
+    return firstValueFrom(
+      this.http
+        .get<ApiResponse<User[]>>(`${apiHost}/users`)
+        .pipe(map(resp => resp.data))
+    );
+  }
+
+  getUserById(id: string) {
+    return firstValueFrom(
+      this.http
+        .get<ApiResponse<User>>(`${apiHost}/users/${id}`)
+        .pipe(map(resp => resp.data))
+    );
+  }
+}
