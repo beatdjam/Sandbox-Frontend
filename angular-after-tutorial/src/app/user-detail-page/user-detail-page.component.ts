a2058b64b29c2f446f429146f18589e51fbdd8be
T.Hori <beat3526@gmail.com>
2022-04-06 23:08:18 +0900

    squash! Angular After Tutorial 1-3. ルーティングと状態管理 ユーザー詳細ページの追加

---
+++ angular-after-tutorial/src/app/user-detail-page/user-detail-page.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnDestroy} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {distinctUntilChanged, map, Subject, takeUntil} from "rxjs";
+import { UserDetailUsecase } from '../user-detail.usecase';
+
+@Component({
+  selector: 'app-user-detail-page',
+  templateUrl: './user-detail-page.component.html',
+  styleUrls: ['./user-detail-page.component.scss']
+})
+export class UserDetailPageComponent implements OnDestroy {
+  user$ = this.userDetailUsecase.user$;
+  private onDestroy$ = new Subject<void>();
+
+  constructor(private route: ActivatedRoute, private userDetailUsecase: UserDetailUsecase) {
+    this.route.params.pipe(
+      takeUntil(this.onDestroy$),
+      map(params => params['userId']),
+      distinctUntilChanged()
+    ).subscribe(userId => this.userDetailUsecase.fetchUser(userId));
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.complete()
+  }
+
+}
