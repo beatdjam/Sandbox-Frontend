c9739c48a0a2caf1108daf4d12b5c4d404ff1f5f
T.Hori <beat3526@gmail.com>
2022-04-29 21:42:44 +0900

    ブログ用サンプル Single State Streamパターン適用

--- Angular-Architecture-Sample/src/app/components/user-list/user-list.component.ts
+++ Angular-Architecture-Sample/src/app/components/user-list/user-list.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import {UserListUsecaseService} from "./user-list-usecase.service";
 import {UserListQuery} from "./user-list.query";
+import {map, Observable} from "rxjs";
+import {combineQueries} from "@datorama/akita";
+import {UserListItem} from "./user-list.store";
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
-  users$ = this.query.selectAll();
-  constructor(private usecase: UserListUsecaseService, private query: UserListQuery) { }
+  readonly state$: Observable<UserStateViewModel>;
+  constructor(private usecase: UserListUsecaseService, private query: UserListQuery) {
+    this.state$ = combineQueries([this.query.selectAll()]).pipe(
+      map(([users]) => new UserStateViewModel(users))
+    );
+  }
 
   ngOnInit(): void {
     this.usecase.fetchUsers();
+  }
+}
+
+
+class UserStateViewModel {
+  constructor(public users: UserListItem[]) {
   }
 }
