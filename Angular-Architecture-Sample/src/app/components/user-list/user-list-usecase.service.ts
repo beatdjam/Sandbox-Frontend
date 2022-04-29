3112c57d877b8496242f699a4a70e63c86069a02
T.Hori <beat3526@gmail.com>
2022-04-29 21:16:05 +0900

    ブログ用サンプル Reformat

--- Angular-Architecture-Sample/src/app/components/user-list/user-list-usecase.service.ts
+++ Angular-Architecture-Sample/src/app/components/user-list/user-list-usecase.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {UsersService} from "../../repository/users.service";
 import {UserListItem, UserListStore} from "./user-list.store";
 import {tap} from "rxjs";
@@ -7,15 +7,16 @@ import {tap} from "rxjs";
   providedIn: 'root'
 })
 export class UserListUsecaseService {
-  constructor(private service: UsersService, private store: UserListStore) { }
+  constructor(private service: UsersService, private store: UserListStore) {
+  }
 
   fetchUsers() {
     this.store.setLoading(true);
     this.service.getAllUsers().pipe(
       tap(res => {
-          const users = res.map(user => {
-            return new UserListItem(user.id, user.first_name,user.last_name);
-          });
+        const users = res.map(user => {
+          return new UserListItem(user.id, user.first_name, user.last_name);
+        });
         this.store.add(users);
       })
     ).subscribe(() => this.store.setLoading(false));
